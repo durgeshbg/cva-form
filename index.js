02cd8956774e51e2a0619de53d31563a8c6dc41d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-11 19:12:00 +0530

    add: zipcode validation

--- index.js
+++ index.js
@@ -14,10 +14,22 @@ function checkEmail(e) {
   if (email.validity.valueMissing) {
     displayErr(email, 'Email is required!');
   } else if (email.validity.typeMismatch) {
-    displayErr(email, 'Not a valid email address');
+    displayErr(email, 'Not a valid email address!');
   } else {
     displayErr(email, '');
   }
 }
 
+function checkZipcode(e) {
+  const pc = e.target;
+  if (pc.validity.valueMissing) {
+    displayErr(pc, 'Zipcode is required!');
+  } else if (pc.validity.patternMismatch) {
+    displayErr(pc, 'Use american postal codes only!');
+  } else {
+    displayErr(pc, '');
+  }
+}
+
 document.querySelector('#email').addEventListener('input', checkEmail);
+document.querySelector('#zipcode').addEventListener('input', checkZipcode);
