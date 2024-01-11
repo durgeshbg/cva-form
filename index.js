3f3ce4a8a9d8e0c93a61c70a9349b64cfe4552c5
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-11 19:02:33 +0530

    add: check msg to style input

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function displayErr(elem, msg) {
-  elem.textContent = msg;
+  elem.nextElementSibling.textContent = msg;
+  if (msg == '') {
+    elem.classList.add('valid');
+    elem.classList.remove('invalid');
+  } else {
+    elem.classList.add('invalid');
+    elem.classList.remove('valid');
+  }
 }
