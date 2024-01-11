9da2476b9e62a1bc7ecc421377c428139ef27805
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-01-11 19:02:51 +0530

    add: validation for email

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function displayErr(elem, msg) {
     elem.classList.remove('valid');
   }
 }
+
+function checkEmail(e) {
+  const email = e.target;
+  if (email.validity.valueMissing) {
+    displayErr(email, 'Email is required!');
+  } else if (email.validity.typeMismatch) {
+    displayErr(email, 'Not a valid email address');
+  } else {
+    displayErr(email, '');
+  }
+}
+
+document.querySelector('#email').addEventListener('input', checkEmail);
